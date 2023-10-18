908e2b7985d4a032364949a8103ec7e9ba3236b0
mateus.santana <mateus.santana@dbserver.com.br>
2023-10-18 16:58:30 -0300

    🐛 Fix: bug on header fixed

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,51 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  atualPath:string = this.router.url;
-  page!:string
   liStyles = {
-    liHome: "",
-    liPost: "",
-    liList: "",
-  }
+    liHome: 'active',
+    liPost: '',
+    liList: '',
+  };
 
-  ngOnInit():void{
-    this.changeListStyle(this.atualPath)
-
-  }
-  constructor(private router: Router){}
-
-
-  changeListStyle(path:string) {
-    switch(path){
-      case "/":
-        console.log("home")
-        this.liStyles.liHome = "active"
-        this.liStyles.liPost = ""
-        this.liStyles.liList = ""
-        break
-      case "/post":
-        console.log("post")
-        this.liStyles.liHome = ""
-        this.liStyles.liPost = "active"
-        this.liStyles.liList = ""
-        break
-      case "/list":
-        console.log("list")
-        this.liStyles.liHome = ""
-        this.liStyles.liPost = ""
-        this.liStyles.liList = "active"
-        break
+  activeStyle(page: string) {
+    switch (page) {
+      case 'home':
+        console.log('home');
+        this.liStyles.liHome = 'active';
+        this.liStyles.liPost = '';
+        this.liStyles.liList = '';
+        break;
+      case 'post':
+        console.log('post');
+        this.liStyles.liHome = '';
+        this.liStyles.liPost = 'active';
+        this.liStyles.liList = '';
+        break;
+      case 'list':
+        console.log('list');
+        this.liStyles.liHome = '';
+        this.liStyles.liPost = '';
+        this.liStyles.liList = 'active';
+        break;
     }
   }
-
-
 }
-
-
